80a1184ab531a32a416b260c8cf85b6ebf97ed5b
alex pelo <alexp20012@gmail.com>
2024-01-25 21:02:59 -0500

    feature - new html to support map building

--- map-builder/src/map components/map.tsx
+++ map-builder/src/map components/map.tsx
@@ -25,7 +25,38 @@ const Map: React.FC = () => {
         }
     },);
 
-    return <div className='map-container' ref={mapRef}></div>;
+        <>
+        <div className='map-container' ref={mapRef} style={{width: "80vw", height: "99vh"}}></div>
+
+        <div className='settings-container'>
+      <div className='sub-container'>
+        <h2>select aspect ratio</h2>
+        <button>3x4</button>
+        <button>4x3</button>
+        <button>16x9</button>
+        <h3>or</h3>
+        <input placeholder='custom' />
+      </div>
+      <div className='sub-container'>
+        <h2>Select Font</h2>
+        <select >
+          <option defaultChecked >Sans Serif</option>
+          <option>placeHolder font</option>
+          <option>placeHolder font</option>
+        </select>
+      </div>
+      <div className='sub-container'>
+        <h2>Icon selection</h2>
+        <select>
+          <option value="1" defaultChecked >default</option>
+          <option value="1">placeHolder</option>
+          <option value="1">placeHolder</option>
+          <option value="1">placeHolder</option>
+        </select>
+      </div>
+      <button className='export-button'>Export</button>
+    </div>
+    </>
 };
 
 export default Map;
